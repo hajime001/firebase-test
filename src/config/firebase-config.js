1198d990dae4232d4bcef2f00f9c9e4bcc494cb9
hajime001 <hajime00000001@gmail.com>
2022-09-08 16:03:45 +0900

    google consoleからコピー

---
+++ src/config/firebase-config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDMRlxHmrG2OJOmOChBGbmk6KK93_TtZXk",
+    authDomain: "fir-test-85ee4.firebaseapp.com",
+    databaseURL: "https://fir-test-85ee4-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fir-test-85ee4",
+    storageBucket: "fir-test-85ee4.appspot.com",
+    messagingSenderId: "945528072096",
+    appId: "1:945528072096:web:2050a3792e83b57e96a6c5",
+    measurementId: "G-FCQK597BW7",    
+}
